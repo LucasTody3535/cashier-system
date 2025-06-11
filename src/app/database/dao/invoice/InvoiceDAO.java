eaeec5f130d7ef3abef32c2dba34e941d933f708
Toodie <lucas.vtodt@gmail.com>
2025-06-11 15:14:42 -0300

    feat(cashier): impl method createTable for InvoiceDAO
    The method creates the Invoices and ProductsIntoInvoice tables

--- src/app/database/dao/invoice/InvoiceDAO.java
+++ src/app/database/dao/invoice/InvoiceDAO.java
@@ -1,5 +1,7 @@
 package app.database.dao.invoice;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import app.database.connection.DBConnection;
@@ -11,6 +13,7 @@ public class InvoiceDAO extends BaseDAO {
 
 	public InvoiceDAO(DBConnection connection) {
 		super(connection);
+		this.createTable();
 	}
 
 	@Override
@@ -32,7 +35,36 @@ public class InvoiceDAO extends BaseDAO {
 	public void search() {}
 
 	@Override
-	protected void createTable() {}
+	protected void createTable() {
+		String sql;
+		DBConnection conn = this.getConnection();
+		PreparedStatement stmt;
+		try {
+			sql = "CREATE TABLE IF NOT EXISTS Invoices("
+					+ "id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,"
+					+ "total FLOAT NOT NULL,"
+					+ "doc VARCHAR(255) NOT NULL,"
+					+ "pdv VARCHAR(255) NOT NULL,"
+					+ "supermarket VARCHAR(255) NOT NULL,"
+					+ "payment_type ENUM('MONEY', 'CREDIT_CARD', 'DEBIT', 'PIX', 'POS') NOT NULL"
+				  + ");";
+			stmt = conn.genPreparedStatement(sql);
+			stmt.execute();
+			stmt.close();
+			sql = "CREATE TABLE IF NOT EXISTS ProductsIntoInvoice("
+					+ "id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,"
+					+ "invoice_id INTEGER NOT NULL,"
+					+ "product_id INTEGER NOT NULL,"
+					+ "FOREIGN KEY (invoice_id) REFERENCES Invoices(id),"
+					+ "FOREIGN KEY (product_id) REFERENCES Products(id)"
+					+ ");";
+			stmt = conn.genPreparedStatement(sql);
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void add(Invoice invoice) {
 		this.invoices.add(invoice);
