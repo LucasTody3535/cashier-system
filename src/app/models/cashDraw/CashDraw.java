bb83df54006595b20e0e53cc5b81ece1b00b4c95
Aigueveda <aigueveda@gmail.com>
2025-06-11 20:33:53 -0300

    feat(cashier): impl class CashDraw

---
+++ src/app/models/cashDraw/CashDraw.java
@@ -0,0 +1,42 @@
+package app.models.cashDraw;
+
+import java.util.Calendar;
+
+public class CashDraw {
+	private long id;
+	private String cashierPdv;
+	private float value;
+	private Calendar date;
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCashierPdv() {
+		return this.cashierPdv;
+	}
+
+	public void setCashierPdv(String cashierPdv) {
+		this.cashierPdv = cashierPdv;
+	}
+
+	public float getValue() {
+		return this.value;
+	}
+
+	public void setValue(float value) {
+		this.value = value;
+	}
+
+	public Calendar getDate() {
+		return this.date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+}
