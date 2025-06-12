e6f7e48e44c959ebdfd84d771f9cd6515d65e226
Toodie <lucas.vtodt@gmail.com>
2025-06-12 14:49:03 -0300

    feat(cashier): impl delete method for PaymentDAO

--- src/app/database/dao/payment/PaymentDAO.java
+++ src/app/database/dao/payment/PaymentDAO.java
@@ -63,6 +63,22 @@ public class PaymentDAO extends BaseDAO {
 
 	@Override
 	public boolean delete(long[] ids) {
+		String sql = "DELETE FROM Payments WHERE id = ?;";
+		DBConnection conn = this.getConnection();
+		PreparedStatement stmt = conn.genPreparedStatement(sql);
+		try {
+			for(long id : ids) {
+				if(id > 0) {
+					stmt.setLong(1, id);
+					stmt.execute();
+					this.payments.removeIf(payment -> id == payment.getId());
+				}
+			}
+			stmt.close();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
