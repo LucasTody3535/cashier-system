e6a344bae1fa30acae2bd3fc50ea5cee48888a55
Toodie <lucas.vtodt@gmail.com>
2025-06-12 15:05:18 -0300

    feat(cashier): impl search method for PaymentDAO

--- src/app/database/dao/payment/PaymentDAO.java
+++ src/app/database/dao/payment/PaymentDAO.java
@@ -1,11 +1,15 @@
 package app.database.dao.payment;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import app.database.connection.DBConnection;
 import app.database.dao.base.BaseDAO;
+import app.enums.payment_type.PaymentType;
+import app.models.invoice.Invoice;
 import app.models.payment.Payment;
 
 public class PaymentDAO extends BaseDAO {
@@ -84,7 +88,34 @@ public class PaymentDAO extends BaseDAO {
 
 	@Override
 	public void search() {
-		
+		String sql = "SELECT payments.id as p_id, payments.date, "
+				+ "invoices.id as i_id, invoices.total, invoices.doc, invoices.pdv, invoices.supermarket, invoices.payment_type "
+				+ "FROM Payments INNER JOIN Invoices ON payments.invoice_id = invoices.id;";
+		DBConnection conn = this.getConnection();
+		PreparedStatement stmt = conn.genPreparedStatement(sql);
+		Payment payment;
+		Invoice invoice;
+		this.payments.clear();
+		try {
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				Calendar cal = Calendar.getInstance();
+				cal.setTimeInMillis(rs.getLong("date"));
+				invoice = new Invoice(
+					rs.getLong("i_id"),
+					null,
+					rs.getFloat("total"),
+					rs.getString("doc"),
+					rs.getString("pdv"),
+					rs.getString("supermarket"),
+					PaymentType.valueOf(PaymentType.class, rs.getString("payment_type"))
+				);
+				payment = new Payment(rs.getLong("p_id"), cal, invoice);
+				this.payments.add(payment);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
