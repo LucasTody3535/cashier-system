90d050251ba96a43d6315eb64d507c07b2a7f969
Aigueveda <aigueveda@gmail.com>
2025-06-12 22:07:19 -0300

    feat(cashier): create login screen frame

--- src/app/ui/screens/login/LoginScreen.java
+++ src/app/ui/screens/login/LoginScreen.java
@@ -1,15 +1,29 @@
 package app.ui.screens.login;
 
+import javax.swing.JFrame;
+
 import app.database.connection.DBConnection;
 import app.interfaces.navigatable_screen.NavigatableScreen;
 
 public class LoginScreen implements NavigatableScreen {
 	private DBConnection connection;
+	private JFrame frame;
 
 	public LoginScreen(DBConnection connection) {
 		this.setConnection(connection);
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+	
 	public DBConnection getConnection() {
 		return this.connection;
 	}
